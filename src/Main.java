e83d3b04fb52d96231c92c80af0a236dacd30b8f
AtividadesPOO <thomas.g@aluno.senai.br>
2026-04-08 21:46:09 -0400

    atividade 2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        // a) Criando o primeiro objeto Carro
-        Carro carro1 = new Carro();
-        // Atribuindo valores usando os métodos Setters (Proteção POO)
-        carro1.setMarca("Toyota");
-        carro1.setModelo("Corolla");
-        carro1.setAno(2022);
 
-        // a) Criando o segundo objeto Carro
-        Carro carro2 = new Carro();
-        carro2.setMarca("Honda");
-        carro2.setModelo("Civic");
-        carro2.setAno(2023);
+        Carro meuCarro = new Carro();
+        meuCarro.setMarca("Chevrolet");
+        meuCarro.setModelo("Onix");
+        meuCarro.setAno(2020);
 
-        // b) Imprimindo os dados de cada carro no console
-        System.out.println("--- Lista de Carros ---");
-        carro1.exibirDados();
-        carro2.exibirDados();
+        System.out.println("--- Testando os Métodos ---");
+
+
+        meuCarro.exibir();
+
+        int idadeAtual = meuCarro.idadeDoCarro();
+        System.out.println("A idade do carro é: " + idadeAtual + " anos.");
     }
 }

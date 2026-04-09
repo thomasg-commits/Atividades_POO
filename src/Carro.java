1d1894a4e13a02765247f7689eace11300de96b3
AtividadesPOO <thomas.g@aluno.senai.br>
2026-04-08 21:51:17 -0400

    atividade 3

--- src/Carro.java
+++ src/Carro.java
@@ -1,23 +1,21 @@
 public class Carro {
 
-    // Atributos encapsulados (privados)
+
     private String marca;
     private String modelo;
     private int ano;
 
-    // a) Método void: Apenas executa uma ação (neste caso, imprimir no console).
-    // A palavra "void" significa "vazio", ou seja, ele não devolve nenhum dado para quem o chamou.
+
     public void exibir() {
         System.out.println("Carro: " + marca + " " + modelo + " (Ano: " + ano + ")");
     }
 
-    // b) Método com retorno: Calcula e devolve um valor inteiro (int).
-    // Ele não imprime nada sozinho, ele "entrega" o número para quem o chamou.
+
     public int idadeDoCarro() {
         return 2026 - ano;
     }
 
-    // --- Getters e Setters (Mantendo a boa prática da POO) ---
+
 
     public String getMarca() {
         return marca;
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
+import java.util.ArrayList; // Importação necessária para usar o ArrayList
+
 public class Main {
     public static void main(String[] args) {
 
 
-        Carro meuCarro = new Carro();
-        meuCarro.setMarca("Chevrolet");
-        meuCarro.setModelo("Onix");
-        meuCarro.setAno(2020);
-
-        System.out.println("--- Testando os Métodos ---");
+        ArrayList<Carro> listaDeCarros = new ArrayList<>();
 
 
-        meuCarro.exibir();
+        Carro c1 = new Carro();
+        c1.setMarca("Fiat");
+        c1.setModelo("Uno");
+        c1.setAno(2010);
+        listaDeCarros.add(c1);
 
-        int idadeAtual = meuCarro.idadeDoCarro();
-        System.out.println("A idade do carro é: " + idadeAtual + " anos.");
+        Carro c2 = new Carro();
+        c2.setMarca("Volkswagen");
+        c2.setModelo("Gol");
+        c2.setAno(2018);
+        listaDeCarros.add(c2);
+
+        Carro c3 = new Carro();
+        c3.setMarca("Ford");
+        c3.setModelo("Mustang");
+        c3.setAno(2024);
+        listaDeCarros.add(c3);
+
+
+        System.out.println("=== Lista de Veículos no Sistema ===");
+        for (Carro c : listaDeCarros) {
+            c.exibir(); // Chama o método exibir de cada objeto individualmente
+        }
     }
 }
